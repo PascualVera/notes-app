7f8ace5a9780919c51be7f632dc64594d9698c7f
PascualVera <pascualveradev@gmail.com>
2022-10-19 10:07:15 +0200

    first junit test created

---
+++ src/main/java/com/notesapp/notesapp/note/Note.java
@@ -0,0 +1,96 @@
+package com.notesapp.notesapp.note;
+
+import java.time.LocalDate;
+
+public class Note {
+	//Note parameter
+	//ID
+	//Title
+	//Text
+	//Date
+	//Date of completion
+	//Link
+	//Mentions
+	//Priority
+	
+	private long id;
+	private String title;
+	private String text;
+	private LocalDate date;
+	private LocalDate dateOfCompletion;
+	private String Link;
+	private String mentions;
+	private String priority;
+	
+	
+	public Note(long id, String title, String text, LocalDate date, LocalDate dateOfCompletion, String link,
+			String mentions, String priority) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.text = text;
+		this.date = date;
+		this.dateOfCompletion = dateOfCompletion;
+		Link = link;
+		this.mentions = mentions;
+		this.priority = priority;
+	}
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	public LocalDate getDateOfCompletion() {
+		return dateOfCompletion;
+	}
+	public void setDateOfCompletion(LocalDate dateOfCompletion) {
+		this.dateOfCompletion = dateOfCompletion;
+	}
+	public String getLink() {
+		return Link;
+	}
+	public void setLink(String link) {
+		Link = link;
+	}
+	public String getMentions() {
+		return mentions;
+	}
+	public void setMentions(String mentions) {
+		this.mentions = mentions;
+	}
+	public String getPriority() {
+		return priority;
+	}
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+	
+	@Override
+	public String toString() {
+		return "Note [id=" + id + ", title=" + title + ", text=" + text + ", date=" + date + ", dateOfCompletion="
+				+ dateOfCompletion + ", Link=" + Link + ", mentions=" + mentions + ", priority=" + priority + "]";
+	}
+	
+	
+}
--- src/test/java/com/notesapp/notesapp/NotesAppApplicationTests.java
+++ src/test/java/com/notesapp/notesapp/NotesAppApplicationTests.java
@@ -1,13 +1,40 @@
 package com.notesapp.notesapp;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.notesapp.notesapp.note.Note;
+
+import org.junit.jupiter.api.Assertions;
+
 @SpringBootTest
 class NotesAppApplicationTests {
-
+	
+	//Note parameter
+	//ID
+	//Title
+	//Text
+	//Date
+	//Date of completion
+	//Link
+	//Mentions
+	//Priority
+	
+	
+	
+	
+	
 	@Test
-	void contextLoads() {
+	void noteCreationSendingStringReturnsNote() {
+		
+		Note note = new Note(0, null, null, null, null, null, null, null);
+		assertNotEquals(note.getTitle(), null);
+		
+		
 	}
 
 }
